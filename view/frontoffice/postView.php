85f98716bea9a04a6e3962fecc9208f2059eaf73
victoragn <victor.agnez@hotmail.fr>
2017-12-18 22:14:48 +0100

    bouton signaler masqué que sur les comments de l'auteur mais modifier et supprimer présents

--- view/frontoffice/postView.php
+++ view/frontoffice/postView.php
@@ -30,16 +30,18 @@ foreach ($comments as &$comment){
 ?>
 <div id="<?= 'divComment'. $comment->getId(); ?>" class='divComment'>
     <p><strong><?= $comment->getUser()->getPseudo(); ?></strong> le <?= $comment->getCommentDate();
-        if(isset($_SESSION['author_id'])&&$_SESSION['author_id']!=$comment->getAuthorId()){
-            if ( $comment->checkSignalByUserId($_SESSION['author_id'])==1 ){?>
-                <a href="index.php?action=post&id=<?= $post->getId(); ?>&signal=off&commentId=<?= $comment->getId(); ?>">
-                    <button class="btnSignalOn">Dé-signaler</button>
-                </a>
-            <?php }else{?>
-                <a href="index.php?action=post&id=<?= $post->getId(); ?>&signal=on&commentId=<?= $comment->getId(); ?>">
-                    <button class="btnSignalOff">Signaler</button>
-                </a>
-            <?php }
+        if(isset($_SESSION['author_id'])){
+            if($_SESSION['author_id']!=$comment->getAuthorId()){
+                if ( $comment->checkSignalByUserId($_SESSION['author_id'])==1 ){?>
+                    <a href="index.php?action=post&id=<?= $post->getId(); ?>&signal=off&commentId=<?= $comment->getId(); ?>">
+                        <button class="btnSignalOn">Dé-signaler</button>
+                    </a>
+                <?php }else{?>
+                    <a href="index.php?action=post&id=<?= $post->getId(); ?>&signal=on&commentId=<?= $comment->getId(); ?>">
+                        <button class="btnSignalOff">Signaler</button>
+                    </a>
+                <?php }
+            }
 
             if($comment->getAuthorId()==$_SESSION['author_id']||$_SESSION['role']==1){
                 ?><button class="btnModifComment" >Modifier</button><button class="btnDeleteComment" >Supprimer</button> <?php
