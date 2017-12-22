6f56e68287f4901982af9c41b8782e3675f6be10
victoragn <victor.agnez@hotmail.fr>
2017-12-22 12:40:54 +0100

    suite de la mise en page

--- view/frontoffice/logHeader.php
+++ view/frontoffice/logHeader.php
@@ -1,11 +1,11 @@
 <div id="logHeader">
     <form method="post" action="#">
         <div>
-            <label for="pseudo">Pseudo</label>
+            <label for="pseudo">Pseudo :</label>
             <input class="champLogHeader" id="pseudo_input" type="text" name="pseudo" value="<?php if(isset($_POST['pseudo'])){echo htmlspecialchars($_POST['pseudo']);}?>" />
             <br />
             
-            <label for="password">Mot de passe</label>
+            <label for="password">Mot de passe :</label>
             <input class="champLogHeader" id="password_input1" type="password" name="password"/>
             <br />
             
