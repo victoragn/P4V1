15e8b0175ab7fb62ce1b6742752e8da391b84abc
victoragn <victor.agnez@hotmail.fr>
2017-12-28 19:29:56 +0100

    Fin de la mise en forme / responsive

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@ $('.btnModifComment').each(function() {/*créer un bouton "modifier" à coté de
     $(this).click( function() {
         var comment=$(this).parents('.divComment').children('.contentComment');
         var idComment=$(this).parents('.divComment').attr('id').substr(10);/*récupère l'id de la div du commentaire pour récuperer l'id du commentaire en question*/
-        $('<form action="index.php?action=updateComment&amp;commentId='+ idComment +'" method="post"><textarea class="textareaModifComment" name="modifComment"></textarea><button>Envoyer la modif</button></form>').insertAfter(comment);/*ajoute le commentaire en question dans une textarea avec un bouton pour envoyer la modif*/
+        $('<form action="index.php?action=updateComment&amp;commentId='+ idComment +'" method="post"><textarea class="textareaModifComment" name="modifComment"></textarea><button class="frontBtn">Envoyer la modif</button></form>').insertAfter(comment);/*ajoute le commentaire en question dans une textarea avec un bouton pour envoyer la modif*/
         $('.textareaModifComment').val(comment.text());
         comment.remove();/*retire le commentaire*/
         $('.btnModifComment').remove();/*retire les autres boutons "modifier" et "supprimer" pour ne pas pouvoir changer plusieurs comment en meme temps*/
